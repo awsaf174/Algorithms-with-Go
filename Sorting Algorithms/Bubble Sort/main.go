a6c23ba12756da59f4770cb860e82f1f5efae1f3
awsaf174 <awsafrahman1704@gmail.com>
2022-09-08 23:49:11 +0200

    added the main and sort functions

---
+++ Sorting Algorithms/Bubble Sort/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	arr := []int{20, 10, 5, 15, 30, 50, 0, 0, 1, 200, 111}
+
+	fmt.Println(sort(arr))
+}
+
+func sort(arr []int) (sortedArray []int) {
+	temp := 0
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr)-1; j++ {
+			if arr[j] > arr[j+1] {
+				temp = arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = temp
+			}
+		}
+	}
+
+	return arr
+}

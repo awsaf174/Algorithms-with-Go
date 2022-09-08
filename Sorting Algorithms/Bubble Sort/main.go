fbeae627f45a7f86aed0b11ce801f1ab42ab46e4
awsaf174 <awsafrahman1704@gmail.com>
2022-09-08 23:55:25 +0200

    refactor: remove temp initialization

--- Sorting Algorithms/Bubble Sort/main.go
+++ Sorting Algorithms/Bubble Sort/main.go
@@ -10,11 +10,10 @@ func main() {
 }
 
 func sort(arr []int) (sortedArray []int) {
-	temp := 0
 	for i := 0; i < len(arr); i++ {
 		for j := 0; j < len(arr)-1; j++ {
 			if arr[j] > arr[j+1] {
-				temp = arr[j]
+				temp := arr[j]
 				arr[j] = arr[j+1]
 				arr[j+1] = temp
 			}
